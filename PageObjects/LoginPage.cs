e956a4fb56b828dd2b07bf4fc932ce19d61f116c
mattcowen <mattcowen@outlook.com>
2020-04-22 15:15:19 +0100

    Add project files.

---
+++ PageObjects/LoginPage.cs
@@ -0,0 +1,98 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium.Support.PageObjects;
+using DynamicsTest.SeleniumHelpers;
+using WebDriverManager.DriverConfigs.Impl;
+using System.Linq;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace DynamicsTest.PageObjects
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+
+            _driver = driver;
+        }
+
+        public IWebElement SignInButton { get; set; }
+
+        public IWebElement NextButton 
+        {
+            get
+            {
+                return _driver.FindElements(By.Id("idSIButton9")).FirstOrDefault();
+            }
+        }
+
+        public IWebElement UserIdField 
+        { 
+            get
+            { 
+                return _driver.FindElements(By.Id("i0116")).FirstOrDefault(); 
+            }
+        }
+
+        public IWebElement PasswordField { get; set; }
+
+
+        ///// <summary>
+        ///// JQuery selector example
+        ///// </summary>
+        //public IWebElement LoginButton => _driver.FindElementByJQuery("input[name='btnSubmit']");
+
+        public void LoginAsAdmin(string baseUrl, string password)
+        {
+
+            // https://crm422752.crm.dynamics.com/
+            _driver.Navigate().GoToUrl(baseUrl);
+
+            UserIdField.Clear();
+            // sending a single quote is not possible with the Chrome Driver, it sends two single quotes!
+            UserIdField.SendKeys("admin@CRM422752.onmicrosoft.com");
+
+            NextButton.Click();
+
+            PasswordField = _driver.FindElements(By.Id("i0118")).FirstOrDefault();
+            PasswordField.Clear();
+            PasswordField.SendKeys(password);
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            wait.Until(f => f.FindElements(By.XPath("//div[@class='col-xs-24 no-padding-left-right button-container']/div/input")).FirstOrDefault());
+
+            SignInButton = _driver.FindElements(By.Id("idSIButton9")).FirstOrDefault();
+            SignInButton.Click();
+
+            var staySignedInDialog = _driver.FindElement(By.XPath("//div[@role='heading']"));
+            if(staySignedInDialog?.Text == "Stay signed in?")
+            {
+                wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+                wait.Until(f => f.FindElements(By.XPath("//div[@class='col-xs-24 no-padding-left-right button-container']/div/input")).FirstOrDefault());
+
+                var noButton = _driver.FindElements(By.Id("idBtn_Back")).FirstOrDefault();
+                noButton.Click();
+            }
+
+        }
+
+        //public void LoginAsNobody(string baseUrl)
+        //{
+        //    _driver.Navigate().GoToUrl(baseUrl);
+        //    SignInLink.Click();
+
+        //    UserIdField.Clear();
+        //    UserIdField.SendKeys("nobody");
+
+        //    PasswordField.Clear();
+        //    PasswordField.SendKeys("blah");
+
+        //    LoginButton.Click();
+        //}
+    }
+}
---
+++ SeleniumHelpers/DriverFactory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using WebDriverManager;
+using System.IO;
+using Microsoft.Edge.SeleniumTools;
+
+namespace DynamicsTest.SeleniumHelpers
+{
+    public enum DriverToUse
+    {
+        InternetExplorer,
+        Chrome,
+        Firefox,
+        EdgeDev
+    }
+
+    public class DriverFactory
+    {
+        private static FirefoxOptions FirefoxOptions
+        {
+            get
+            {
+                var firefoxProfile = new FirefoxOptions();
+                firefoxProfile.SetPreference("network.automatic-ntlm-auth.trusted-uris", "http://localhost");
+                return firefoxProfile;
+            }
+        }
+
+        public IWebDriver Create()
+        {
+            IWebDriver driver;
+            var driverToUse = DriverToUse.EdgeDev; //ConfigurationHelper.Get<DriverToUse>("DriverToUse");
+
+            switch (driverToUse)
+            {
+                case DriverToUse.EdgeDev:
+                    //new DriverManager().SetUpDriver(
+                    //    "https://msedgedriver.azureedge.net/83.0.478.10/edgedriver_win64.zip",
+                    //    Path.Combine(Directory.GetCurrentDirectory(), "msedgedriver.exe"),
+                    //    "msedgedriver.exe"
+                    //);
+                    var options = new EdgeOptions();
+                    options.UseChromium = true;
+                    options.BinaryLocation = @"C:\Program Files (x86)\Microsoft\Edge Dev\Application\msedge.exe";
+
+                    driver = new EdgeDriver(options);
+                    
+                    break;
+                case DriverToUse.InternetExplorer:
+                    driver = new InternetExplorerDriver(AppDomain.CurrentDomain.BaseDirectory, new InternetExplorerOptions(), TimeSpan.FromMinutes(5));
+                    break;
+                case DriverToUse.Firefox:
+                    var firefoxProfile = FirefoxOptions;
+                    driver = new FirefoxDriver(firefoxProfile);
+                    driver.Manage().Window.Maximize();
+                    break;
+                case DriverToUse.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            driver.Manage().Window.Maximize();
+            var timeouts = driver.Manage().Timeouts();
+
+            timeouts.ImplicitWait = TimeSpan.FromSeconds(30);
+            timeouts.PageLoad = TimeSpan.FromSeconds(60);
+
+            // Suppress the onbeforeunload event first. This prevents the application hanging on a dialog box that does not close.
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.onbeforeunload = function(e){};");
+            return driver;
+        }
+    }
+}
